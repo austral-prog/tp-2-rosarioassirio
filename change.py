368b09d7b83dd920b7523d8d8c74bc7c44deb6dd
rosarioassirio <162721687+rosarioassirio@users.noreply.github.com>
2024-03-26 10:24:07 -0300

    change.py

--- change.py
+++ change.py
@@ -1,3 +1,8 @@
 def change():
     expense = 23.75
     money = 100
+    print ( "Ingresar gasto:" \n expense )
+    print ( "Dinero recibido:" \n money \n )
+    print ( "Vuelto" \n )
+    print ( "Pesos:" \n int(money-expense) )
+    print ( "Centavos:" \n (money-expense) - int(money-expense) )
